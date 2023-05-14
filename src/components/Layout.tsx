9c50cc6fad44147f9998112fa3abafcf004b03ba
Ilya Lanov <42113015+laaanov@users.noreply.github.com>
2023-05-14 05:55:12 +0300

    dsadsad

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -18,7 +18,7 @@ const Layout = ({ children }: PropsWithChildren) => {
         <title>Внештата | Социальная сеть для профессионалов</title>
         <meta name="description" content="Описание внештата." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.png" />
       </Head>
 
       <div className="min-h-screen">
